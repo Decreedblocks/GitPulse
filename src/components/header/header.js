df00204124b4e13c5dafb2ded93b348fb7491ee4
Decreedblocks <emekky54@gmail.com>
2023-06-05 11:57:56 +0100

    added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
     <h2 className="title"><a className="Headerlink" href="../public/index.html">Github Profile Analyzer</a></h2>
-    <h3 className="header-li"><a className="Headerlink" href="https://github.com/AnubhavSrivastavaGithub/gh_analyzer" target="blank">About</a></h3>
+    <h3 className="header-li"><a className="Headerlink" href="https://github.com/Decreedblocks/GitPulse" target="blank">About</a></h3>
     </header>
   );
 }
